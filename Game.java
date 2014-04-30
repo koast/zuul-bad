05e23b713d82ed200d087ac22eaead2af6cbe016
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-04-30 12:52:52 +0200

    añadido comando look para mirar la informacion local

--- Game.java
+++ Game.java
@@ -146,6 +146,9 @@ public class Game
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
         }
+        else if (commandWord.equals("look")) {
+            printLocationInfo();
+        }
 
         return wantToQuit;
     }
