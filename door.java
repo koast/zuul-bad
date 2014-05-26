b62702ffbb66155855144ea9efd42c47f4f0d2f4
Alejandro Aller <Alejandroallerdiez@gmail.com>
2014-05-26 10:14:23 +0200

    creada clase puerta

---
+++ door.java
@@ -0,0 +1,18 @@
+public class door
+{
+    private boolean open;
+    
+    public door(boolean open)
+    {
+    }
+    
+    public boolean getOpen()
+    {
+        return open;
+    }
+    
+    public void open()
+    {
+        open = true;
+    }
+}
